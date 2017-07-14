18072ce7a6bce886192ba7ac8ec8e71aab7fd889
Emil Milanov <emo_mlnv@yahoo.com>
2017-07-14 15:17:40 +0200

    Simple Linear Regression with sklearn

--- 02-linear_regression/script.py
+++ 02-linear_regression/script.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn import linear_model
 
 # Datensatz laden und Spalten benennen
 df = pd.read_csv("fish.csv")
@@ -14,7 +15,25 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(xs=df.alter, ys=df.wassertemperatur, zs=df.laenge)
 
+# Trainings- und Testdaten
+test = df.sample(int(len(df)*0.2))
+train = df.drop(df.index[test.index])
+
+# Lineare Regression mit scikit-learn
+x_train = np.array(train[['alter', 'wassertemperatur']])
+y_train = np.array(train.laenge)
+
+x_test = np.array(test[['alter', 'wassertemperatur']])
+y_test = np.array(test.laenge)
+
+regr = linear_model.LinearRegression()
+
+regr.fit(x_train, y_train)
+
+print('Coefficients: ', regr.coef_)
+print("Mean squared error: %.2f" % np.mean((regr.predict(x_test) - y_test) ** 2))
+print('Variance score: %.2f' % regr.score(x_train, y_train))
+
+ax.scatter(xs=x_test[:,0], ys=x_test[:,1], zs=regr.predict(x_test))
+
 plt.show()
-
-
-
